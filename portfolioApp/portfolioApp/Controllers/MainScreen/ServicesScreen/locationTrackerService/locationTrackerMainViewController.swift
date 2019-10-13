72b6c0bd9e1c16b26ac5ee4304d5db319ef3ba96
Stephenson Ang <stiph@Stephensons-MacBook-Pro.local>
2019-10-13 18:16:38 +0800

    Add feature in servies and change the flow of locationTracker feature

--- portfolioApp/portfolioApp/Controllers/MainScreen/ServicesScreen/locationTrackerService/locationTrackerMainViewController.swift
+++ portfolioApp/portfolioApp/Controllers/MainScreen/ServicesScreen/locationTrackerService/locationTrackerMainViewController.swift
@@ -1,14 +1,14 @@
 //
-//  ServiceViewController.swift
+//  locationTrackerMainViewController.swift
 //  portfolioApp
 //
-//  Created by Stephenson Ang on 05/10/2019.
+//  Created by Stephenson Ang on 10/13/19.
 //  Copyright © 2019 Stephenson Ang. All rights reserved.
 //
 
 import UIKit
 
-class ServiceViewController: UIViewController {
+class locationTrackerMainViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -16,11 +16,16 @@ class ServiceViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
+    @IBAction func backBtnWasPressed(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
     @IBAction func locationTrackerBtnWasPressed(_ sender: Any) {
         
-        guard let locationTracker = self.storyboard?.instantiateViewController(withIdentifier: "locationTrackerMainViewController") else { return }
+        guard let locationTracker = self.storyboard?.instantiateViewController(withIdentifier: "locationTrackerViewController") else { return }
         locationTracker.modalPresentationStyle = .fullScreen
         self.present(locationTracker, animated: true)
         print("BUTTON PRESSED")
     }
+
 }
