4a8085d9e9b8f5d11f783d79227c977ab1cad5ee
Stephenson Ang <stiph@Stephensons-MacBook-Pro.local>
2019-10-19 14:18:06 +0800

    pod install and center google maps location

--- portfolioApp/portfolioApp/Controllers/MainScreen/ServicesScreen/locationTrackerService/locationTracker/locationTrackerViewController.swift
+++ portfolioApp/portfolioApp/Controllers/MainScreen/ServicesScreen/locationTrackerService/locationTracker/locationTrackerViewController.swift
@@ -9,28 +9,27 @@
 import UIKit
 import GoogleMaps
 
-class locationTrackerViewController: UIViewController {
+class locationTrackerViewController: UIViewController, GMSMapViewDelegate {
     
     @IBOutlet weak var mapView: GMSMapView!
     
-    var locationManager = CLLocationManager()
+    let locationManager = CLLocationManager()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         mapView.viewLayout()
         
-        self.mapView.delegate = self as? GMSMapViewDelegate
+        self.mapView.delegate = self
         self.mapView.isMyLocationEnabled = true
+        self.mapView.settings.myLocationButton = true
         
-        locationManager = CLLocationManager()
         locationManager.desiredAccuracy = kCLLocationAccuracyBest
         locationManager.requestAlwaysAuthorization()
         locationManager.distanceFilter = 10
         locationManager.startUpdatingLocation()
         locationManager.delegate = self
         
-        self.locationManager.startUpdatingLocation()
     }
     
     @IBAction func backBtnWasPressed(_ sender: Any) {
@@ -43,14 +42,17 @@ class locationTrackerViewController: UIViewController {
 
 }
 extension locationTrackerViewController: CLLocationManagerDelegate {
-    
+
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         let location = locations.last!
         
-        let camera = GMSCameraPosition.camera(withLatitude: location.coordinate.latitude, longitude: location.coordinate.longitude, zoom: 10.0)
-        self.mapView.animate(to: camera)
-        
+        print("\(location.coordinate.latitude) -- \(location.coordinate.longitude)")
+
+        let cameraPosition = GMSCameraPosition.camera(withLatitude: location.coordinate.latitude, longitude: location.coordinate.longitude, zoom: 18.0)
+        let cameraUpdate = GMSCameraUpdate.setCamera(cameraPosition)
+        self.mapView.moveCamera(cameraUpdate)
+
         self.locationManager.stopUpdatingLocation()
     }
-    
+
 }
